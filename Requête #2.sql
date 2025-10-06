3afdc2790e84da042cf94e54ed1e25254e171b33
f-gehin-crm <183800959+f-gehin-crm@users.noreply.github.com>
2025-10-06 12:36:48 +0200

    second

---
+++ Requête #2.sql
@@ -0,0 +1,8 @@
+INSERT INTO rs_users (username, email, password_hash) VALUES 
+ ('fred',' fred@gmail;com', 'bob');rs_users
+ 
+ SELECT*FROM rs_users;
+ 
+ SELECT * FROM rs_users WHERE id = 1;
+ 
+ SELECT username, email FROM rs_users;   
